60db4b0a5eed016c8eed5a88edaecf383be52d43
juntestaccount <sinisa@gmx.com>
2016-06-09 01:10:18 -0500

    Delete phpscript.php

--- views/js/phpscript.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-    header("Content-type: text/javascript; charset: UTF-8");
-    header("Cache-Control: must-revalidate");
-    $offset = 60;
-    $ExpStr = "Expires: " .
-    gmdate("D, d M Y H:i:s",
-    time() + $offset) . " GMT";
-    header($ExpStr);
-?>
